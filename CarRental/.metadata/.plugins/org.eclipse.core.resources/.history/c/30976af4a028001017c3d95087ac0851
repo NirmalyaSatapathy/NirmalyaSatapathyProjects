6dfd31ab82c16095f1a8415b57e81e89b628e86e
Nirmalya <nirmalyasuman@gmai.com>
2025-06-04 16:43:33 +0530

    training project

--- CarRental/.metadata/.plugins/org.eclipse.core.resources/.history/c/30976af4a028001017c3d95087ac0851
+++
@@ -1,8 +0,0 @@
-package com.java.Carrental.Exception;
-
-public class Uexception extends Exception{
-	public Uexception(String message)
-	{
-		super(message);
-	}
-}
---
+++ SplitExpense/src/main/java/com/java/jsf/UserDao.java
@@ -0,0 +1,19 @@
+package com.java.jsf;
+
+import java.util.List;
+
+public interface UserDao {
+	String addUserDao(Users user);
+
+	List<Users> showUserDao();
+
+	String addGroup(TravelGroup travelGroup);
+
+	String addGroupMembers(GroupMembers groupMember);
+
+	String addDailyExpenses(DailyExpenses dailyExpenses);
+
+	List<DailyExpenses> showAllExpenses(int groupId);
+	List<TravelGroup> showGroups();
+	List<GroupMembers> showGroupMembers();
+}
